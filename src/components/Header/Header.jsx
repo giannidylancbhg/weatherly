1dacd81cb9f85a2b7e21ae33004575d8994b491d
Gianni Dylan <giannidylancbhg@gmail.com>
2026-07-31 00:45:32 +0800

    feat: keep search icon highlighted when input has text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Header() {
+  const [location, setLocation] = useState("");
   return (
     <header className="header">
       <div className="brand">
@@ -11,11 +13,16 @@ export default function Header() {
 
         <form className="searchbar">
           <label className="search-input">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className={`search-icon${location.trim() ? "has-text" : ""}`}
+            />
             <input
               type="text"
               placeholder="Type a location"
               id="search-input"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
             />
           </label>
 
