51b2f20eb5a953342127a67c74c820b8b937b65a
Gianni Dylan <giannidylancbhg@gmail.com>
2026-07-31 19:42:54 +0800

    feat: layout CurrentWeather component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,3 +1,10 @@
+import "./Weather.css";
+
 export default function Weather() {
-  return <main></main>;
+  return (
+    <main className="weather-container">
+      <section className="current-weather"></section>
+      <section className="forecast"></section>
+    </main>
+  );
 }
